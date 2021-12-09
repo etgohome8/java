a9bfffeb9c4a48a2c0fb180726b260c964531004
etgohome8 <86273582+etgohome8@users.noreply.github.com>
2021-12-09 12:18:17 +0900

    Add files via upload

---
+++ java-class-01/src/folder/HelloJava.java
@@ -0,0 +1,27 @@
+package folder;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// 주석
+		// 주석은 프로그램에 영향을 주지 않는다.
+		// 주석은 소스내용 설명을 위한 코드.(즉 개발자를 위한 코드)
+		// 한줄주석
+		/* 범위주석 */
+
+		System.out.println("Hello~~ Java!!");
+		// Hello~~ Java!! 를 출력하는 코드입니다.
+		System.out.println("/*주석이 될 수 없습니다.*/");
+
+		// 이클립스에서 자주 사용하는 단축키!
+		// tab : 들여쓰기
+		// shift + tab : 내어쓰기
+		// ctrl + shift + c : 라인주석 단축키
+		// ctrl + (+, -) : 폰트 크기조절 단축키
+		// ctrl + space : 자동완성
+		// ex) sysout 후 ctrl + space >> System.out.println();
+		// ctrl + shift + f : 자동정렬
+
+	}
+
+}
---
+++ java-class-01/src/folder/Sample.java
@@ -0,0 +1,24 @@
+package folder;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		// 연습해보겠습니다.
+		System.out.println("문자열 입니다.");
+		System.out.println("문자열 안에 내용은 그대로 출력이 됩니다.");
+		System.out.println("안녕하세요");
+		// () 안에 내용을 출력하고 줄바꿈
+		
+		System.out.print("안녕");
+		System.out.print("하세요");
+		// () 안에 내용을 출력하고 줄바꿈X
+		
+		System.out.println("1+5");
+		// ""안에 넣은 내용은 그대로 출력
+		System.out.println(1 + 5);
+		// () 안에 값을 계산 후 6을 출력
+		
+	}
+
+}
